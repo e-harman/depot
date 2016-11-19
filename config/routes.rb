d4ec05026ce216d226fb1be8a62a98b478803268
e-harman <harman.eric@gmail.com>
2016-11-19 11:38:03 -0800

    Chapter 12: iteration G2, final

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :orders
-  get 'store/index'
-  
   resources :line_items
   resources :carts
-  resources :products
+  
+  get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
 
  
   # The priority is based upon order of creation: first created -> highest priority.
