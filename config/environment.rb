d165092fe811309c2ca25b9233ab4c6bdafcd27c
e-harman <harman.eric@gmail.com>
2016-11-20 15:07:03 -0800

    Chapter 13: iteration H1

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,19 @@ Rails.application.initialize!
 # To configure email
 # Though, in routes.rb it starts with:
 # "Rails.application.routes.draw do"
+# This may be a rails 4.0 vs. 4.2 thing
 # where the book shows Depot::
 # So should this be Rails.application.configure?
-# This may be a rails 4.0 vs. 4.2 thing
 
-Depot::Application.configure do
+# Book says:
+# Depot::Application.configure do
+#   config.action_mailer.delivery_method = :test
+# end
+
+# So, there is a possibility it should be: 
+
+Rails.application.configure do
   config.action_mailer.delivery_method = :test
 end
+# See: 
+# http://stackoverflow.com/questions/20071022/rails-email-with-gmail-smtp-error-errnoeconnrefused-no-connection-could-be
