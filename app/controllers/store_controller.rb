3dc1733b79e9142975b825a8c1abae665aa8fd36
e-harman <harman.eric@gmail.com>
2016-11-26 17:49:19 -0800

    Chapter 15: iteration J4

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,11 +2,17 @@ class StoreController < ApplicationController
   skip_before_action :authorize
   include CurrentCart
   before_action :set_cart
+
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
     @time = Time.now
     index_view_counter
   end
+
   def index_view_counter
     if session[:counter].nil?
       session[:counter] = 0
